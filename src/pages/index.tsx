a7606e0c377f19566fe788e7a575c78a568ec6a0
Jay Liu <jliu2589@gmail.com>
2023-08-19 21:59:03 -0400

    Mobile menu dropdown. Covers stuff below

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image';
 import Header from '../components/header';
 import Showcase from '../components/showcase';
 import Pricing from '../components/pricing';
+import { FaBars } from 'react-icons/fa';
 
 //header
-import { FaBars } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { BiX } from 'react-icons/bi';
 
 //accordian
 import {
@@ -21,30 +22,62 @@ import { client } from '@/lib/sanity';
 
 export default function Home({ data }) {
   console.log(data);
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  useEffect(() => {
+    if (isMobileMenuOpen) {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
+  }, [isMobileMenuOpen]);
+
   return (
     <main className=' h-screen'>
       <header className='top-0 border-b-2 border-gray-200 h-16  '>
         <nav className='flex justify-between max-w-7xl items items-center mx-auto'>
           <div>Logo</div>
-          <ul className='flex space-x-7'>
-            <li>About Us</li>
-            <li>Pricing</li>
-            <li>FAQ</li>
-            <li>Contact Us</li>
-          </ul>
+          <div className='pr-4 pt-4'>
+            <ul className='hidden sm:flex space-x-7'>
+              <li>About Us</li>
+              <li>Pricing</li>
+              <li>FAQ</li>
+              <li>Contact Us</li>
+            </ul>
+            <button
+              className='block sm:hidden'
+              onClick={(e) => {
+                e.stopPropagation(); // Prevent the event from bubbling to the document
+                setIsMobileMenuOpen(!isMobileMenuOpen);
+              }}
+            >
+              <FaBars />
+            </button>
+          </div>
         </nav>
       </header>
-      <div>
-        <div className='flex justify-between max-w-7xl items items-center'>
-          <div className='flex flex-col justify-center items-center'>
-            <h1 className='text-5xl font-bold text-gray-800'>
-              We are a modern digital agency
-            </h1>
-            <div></div>
-            <div></div>
+      <div className='relative'>
+        {isMobileMenuOpen && (
+          <div className='absolute border border-black bg-white z-10 w-full'>
+            <ul>
+              <li className='text-center border-b'>About Us</li>
+              <li className='text-center border-b'>Pricing</li>
+              <li className='text-center border-b'>FAQ</li>
+              <li className='text-center'>Contact Us</li>
+            </ul>
           </div>
-        </div>
+        )}
       </div>
+      <div>showcase</div>
+      <div>Solar Panels </div>
+      <div>Why Us</div>
+      <div>contact us</div>
+      <div>faq</div>
     </main>
   );
 }
